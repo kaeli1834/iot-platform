7b1012c4f9ff059bbab6e93d26960ddd9b70115a
Kaeli <122753991+kaeli1834@users.noreply.github.com>
2025-12-16 16:21:45 +0100

    clean

--- backend/src/reading-values/dto/update-reading-value.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateReadingValueDto } from './create-reading-value.dto';
-
-export class UpdateReadingValueDto extends PartialType(CreateReadingValueDto) {}
